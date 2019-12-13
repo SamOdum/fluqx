9a15aafe304db0171d2e6fcd0001599ea3d2cfa0
Sam Odum <sam.odum1@gmail.com>
2019-12-13 10:39:09 +0100

    ch(/client): initialize project for development

---
+++ client/src/components/MenuBtn.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/MenuBtn.scss'
+
+function MenuBtn() {
+
+    return (
+        <div className='menu-btn'>
+            <div className="btn-line"></div>
+            <div className="btn-line"></div>
+            <div className="btn-line"></div>
+        </div>
+    )
+}
+
+export default MenuBtn
